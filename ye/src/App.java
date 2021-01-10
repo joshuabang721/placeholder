ec3ace94845d4266a61b380b0815dba53ba6e159
joshuabang721 <77245378+joshuabang721@users.noreply.github.com>
2021-01-10 14:21:19 -0500

    Add files via upload

---
+++ ye/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Whats up gamers!");
+    }
+    public int addTwoNumbers(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Whats up gamers!");
+    }
+}
+
